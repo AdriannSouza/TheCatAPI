55fe7d775746d861b5741a9c8f2dd23e84f5e7d1
Adrian Faria de Souza <116755380+AdriannSouza@users.noreply.github.com>
2023-06-29 13:03:38 -0300

    Usabilidade

--- catAPI.js
+++ catAPI.js
@@ -1,41 +1,35 @@
-var btn = document.querySelector('.random');
-btn.addEventListener('click', function () { exibe() });
+var btn = document.querySelector('.random')
+btn.addEventListener('click', function () { exibe() })
+
 
 // Requisição da WEB
 async function fetchData() {
     let data = [];
     try {
-        const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+        const response = await fetch("https://api.thecatapi.com/v1/images/search");
+
         data = await response.json();
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
+
+    } catch (error) { console.error(error); return [] }
+
     return data;
 }
 
 async function exibe() {
+
     try {
-        const gatos = await fetchData();
+        const gato = await fetchData();
         let sec_cats = document.querySelector('.cats');
         
-        for (let i = 0; i < gatos.length; i++) {
-            const eCat = gatos[i];
-            console.log(eCat);
+        gato.forEach(eCat => {
             const catCardHTML = `
                 <div class="cat-card">
                     <img class="foto" src="${eCat.url}" alt="">
                 </div>
-            `;
+                `;
+            console.log(eCat);
             sec_cats.insertAdjacentHTML('afterbegin', catCardHTML);
-            await delay(3000); // Aguarda 5 segundos antes de exibir a próxima imagem
-        }
+        });
 
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    } catch (error) { console.error(error) }
 }
